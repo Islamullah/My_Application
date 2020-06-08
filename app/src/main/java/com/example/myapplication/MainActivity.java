b86f6d87dcbf9de6c20c3ffe6f13941ce378db25
Ghazali <softprog9@gmail.com>
2020-06-08 16:22:39 +0500

    New Commit with text View initialization

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // this is comment. should push this comment. testing.
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+
+
     }
 }
